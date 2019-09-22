2fc5a626697868d36dff4a24369480935a9de7d6
Radu Toader <radu.m.toader@gmail.com>
2019-09-22 15:30:29 +0300

    save

--- varnish/entrypoint.sh
+++ varnish/entrypoint.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
-set -euo pipefail
+#set -euo pipefail
 
-if [ ! -z "$VARNISH_BACKEND_HOST" ] ;then
+if [ ! -z "${VARNISH_BACKEND_HOST}" ] ;then
     host_replace_expression=".host = \".+\"/.host = \"${VARNISH_BACKEND_HOST}\""
     port_replace_expression=".port = \".+\"/.port = \"${VARNISH_BACKEND_PORT:-80}\""
     perl -pi -e "s/${host_replace_expression}/g; s/${port_replace_expression}/g" /etc/varnish/default.vcl
+fi
+
+SECRET_FILE=/etc/varnish/secret
+if test -f "$SECRET_FILE"; then
+    echo "$SECRET_FILE exists"
+else
+    echo "$SECRET_FILE does not exists"
+    UUID=$(cat /proc/sys/kernel/random/uuid)
+    echo $UUID > $SECRET_FILE
+    echo "Using $UUID secret"
 fi
 
 varnishd \
@@ -13,5 +23,7 @@ varnishd \
     -f "/etc/varnish/default.vcl" \
     -S "/etc/varnish/secret" \
     -s "${VARNISH_CACHE_STORE:-malloc,512m}" \
-    -p "feature=+esi_disable_xml_check" -p "vcc_allow_inline_c=on" -p "cli_buffer=32768" \
-    -F
+    -p "feature=+esi_disable_xml_check" \
+    -p "vcc_allow_inline_c=on"
+#    -p "cli_buffer=32768" \
+#    -F
